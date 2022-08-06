7f4b7dfb83264500e1a2756d8ccf25621ab8c05f
Marcos Leite <marcosvpleite@gmail.com>
2022-08-06 15:59:51 -0300

    requisito 3

---
+++ src/pages/Settings.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <div>
+        <header data-testid="settings-title">
+          <h2>Settings</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Settings;
