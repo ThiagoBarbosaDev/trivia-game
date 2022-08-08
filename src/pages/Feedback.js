2ebe51f8d9291b333eb277e79530f442404ca638
rubimeryka <erykarubim13@gmail.com>
2022-08-08 18:27:55 -0300

    requisito 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from '../components/Button';
 import Header from '../components/Header';
 
 class Feedback extends Component {
@@ -10,6 +11,11 @@ class Feedback extends Component {
     const feedback = acertos > 2 ? 'Well Done!' : 'Could be better...';
     return feedback;
   };
+
+  handlePlayAgainClick = () => {
+    const { history: { push } } = this.props;
+    push('/');
+  }
 
   render() {
     const { acertos } = this.props;
@@ -26,6 +32,12 @@ class Feedback extends Component {
         <p data-testid="feedback-total-score">
           {`Um total de ${acertos * multiplicação} pontos`}
         </p>
+        <Button
+          dataTestId="btn-play-again"
+          onClick={ () => this.handlePlayAgainClick() }
+        >
+          Play Again
+        </Button>
       </div>
     );
   }
@@ -37,6 +49,7 @@ const mapStateToProps = ({ playerReducer: { assertions } }) => ({
 
 Feedback.propTypes = {
   acertos: PropTypes.number.isRequired,
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
 };
 
 export default connect(mapStateToProps, null)(Feedback);
